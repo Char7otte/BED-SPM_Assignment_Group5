eab4a9ae5ae49a1cfef895ff3309f82a493c79eb
Harangad Singh <s10267942@connect.np.edu.sg>
2025-07-04 20:34:37 +0800

    Added code for logic of another feature, create medication.

--- medication_tracker/model/medTrackerModel.js
+++ medication_tracker/model/medTrackerModel.js
@@ -56,7 +56,43 @@ async function getWeeklyMedications(startDate, endDate) {
     }
 }
 
+async function createMedication(medicationDate) {
+    let connection;
+    try {
+        connection = await sql.connect(dbConfig);
+        const query = "INSERT INTO Medications (UserID, MedicationName, MedicationDate, MedicationTime, MedicationDosage, MedicationNotes, MedicationReminders, PrescriptionStartDate, PrescriptionEndDate, IsTaken) VALUES (@userID, @medicationName, @medicationDate, @medicationTime, @medicationDosage, @medicationNotes, @medicationReminders, @prescriptionStartDate, @prescriptionEndDate, @isTaken)";
+        const request = connection.request();
+        request.input("userID", sql.Int, medicationDate.userID);
+        request.input("medicationName", sql.NVarChar, medicationDate.medicationName);
+        request.input("medicationDate", sql.Date, medicationDate.medicationDate);
+        request.input("medicationTime", sql.Time, medicationDate.medicationTime);
+        request.input("medicationDosage", sql.NVarChar, medicationDate.medicationDosage);
+        request.input("medicationNotes", sql.NVarChar, medicationDate.medicationNotes);
+        request.input("medicationReminders", sql.NVarChar, medicationDate.medicationReminders);
+        request.input("prescriptionStartDate", sql.Date, medicationDate.prescriptionStartDate);
+        request.input("prescriptionEndDate", sql.Date, medicationDate.prescriptionEndDate);
+        request.input("isTaken", sql.Bit, medicationDate.isTaken);
+        const result = await request.query(query);
+        return result.recordset[0];
+    }
+    catch (error) {
+        console.error("Database error:", error);
+        throw error;
+    } 
+    finally {
+        if (connection) {
+            try {
+                await connection.close();
+            } 
+            catch (err) {
+                console.error("Error closing connection:", err);
+            }
+        }
+    }
+}
+
 module.exports = {
     getDailyMedications,
-    getWeeklyMedications
+    getWeeklyMedications,
+    createMedication
 };
