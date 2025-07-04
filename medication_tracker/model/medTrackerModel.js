77159ab8607f982782921245effc63c534cf7062
Harangad Singh <s10267942@connect.np.edu.sg>
2025-07-04 17:36:30 +0800

    Added code for another function, view weekly medications. Changed some code in getDailyMedication feature as well after understanding an error.

--- medication_tracker/model/medTrackerModel.js
+++ medication_tracker/model/medTrackerModel.js
@@ -7,7 +7,36 @@ async function getDailyMedications(date) {
         connection = await sql.connect(dbConfig);
         const query = "SELECT U.Name, M.MedicationName, M.MedicationDate, M.MedicationTime, M.MedicationDosage, M.MedicationNotes M.MedicationReminders, M.PrescriptionStartDate, M.PrescriptionEndDate, M.IsTaken FROM Medications M JOIN Users U ON M.UserID = U.UserID WHERE U.Name = @name";
         const request = connection.request();
-        request.input("name", sql.NVarChar, date);
+        request.input("name", sql.NVarChar, name);
+        request.input("date", sql.Date, date);
+        const result = await request.query(query);
+        return result.recordset;
+    }
+    catch (error) {
+        console.error("Database error:", error);
+        throw error;
+    } 
+    finally {
+        if (connection) {
+            try {
+                await connection.close();
+            } 
+            catch (err) {
+                console.error("Error closing connection:", err);
+            }
+        }
+    }
+}
+
+async function getWeeklyMedications(startDate, endDate) {
+    let connection; 
+    try {
+        connection = await.sql.connect(dbConfig);
+        const query = "SELECT U.Name, M.MedicationName, M.MedicationDate, M.MedicationTime, M.MedicationDosage, M.MedicationNotes, M.MedicationReminders, M.PrescriptionStartDate, M.PrescriptionEndDate, M.IsTaken FROM Medications M JOIN Users U ON M.UserID = U.UserID WHERE U.Name = @name AND M.MedicationDate BETWEEN @startDate AND @endDate";
+        const request = connection.request();
+        request.input("name", sql.NVarChar, name);
+        request.input("startDate", sql.Date, startDate);
+        request.input("endDate", sql.Date, endDate);
         const result = await request.query(query);
         return result.recordset;
     }
@@ -28,5 +57,6 @@ async function getDailyMedications(date) {
 }
 
 module.exports = {
-    getDailyMedications
+    getDailyMedications,
+    getWeeklyMedications
 };
