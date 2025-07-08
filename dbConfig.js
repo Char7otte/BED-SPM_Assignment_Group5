aa7cf840555c7489d6c8f64cbd7c6cb93162d524
shar <faethychan@gmail.com>
2025-07-08 13:52:20 +0800

    Fix dotenv

--- dbConfig.js
+++ dbConfig.js
@@ -1,11 +1,11 @@
 module.exports = {
-    user: "12345",
-    password: "12345",
-    server: "DESKTOP-4RFAEEN/SQLEXPRESS",
-    database: "bed_spm",
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    server: process.env.DB_SERVER,
+    database: process.env.DB_DATABASE,
     trustServerCertificate: true,
     options: {
         port: parseInt(process.env.DB_PORT), // Default SQL Server port
-        connectionTimeout: 60000, // Connection timeout in milliseconds
+        connectionTimeout: 15000, // Connection timeout in milliseconds
     },
 };
