b1985138790fe5d492db5e7a34d403393b0f2645
shar <faethychan@gmail.com>
2025-08-10 19:05:23 +0800

    Remove print statement

--- cat/catAPI.js
+++ cat/catAPI.js
@@ -10,7 +10,6 @@ async function getCatImg(req, res) {
     const limit = req.params.limit || "10";
     // if (limit > 10) limit = 10;
     const response = await axios.get(`https://api.thecatapi.com/v1/images/search?limit=${limit}`, options);
-    console.log(response.data);
     const data = response.data;
     return res.render("cat/cat.ejs", { data });
 }
